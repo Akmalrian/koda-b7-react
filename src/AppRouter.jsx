c689471fe9eebc2c8b74e28d31c667114d5edde7
Akmal Rian <akmalrian@gmail.com>
2026-04-02 22:55:06 +0700

    feat : add AppRouter for main routing browser

---
+++ src/AppRouter.jsx
@@ -0,0 +1,56 @@
+// import React from "react";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Home from "./pages/Home";
+import AppPokemon from "./pages/AppPokemon";
+import App from "./pages/App";
+import ProductApp from "./pages/ProductApp";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Review from "./pages/Review";
+
+function AppRouter(){
+  return(
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/increase&decrease" element={<App />}/>
+      <Route path="/product" element={<ProductApp />}/>
+      <Route path="/pokemon" element={<AppPokemon />}/>
+      <Route path="/review" element={<Review />}/>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
+
+// class AppRouter extends React.Component {
+//   state = {
+//     currentPage: "home", 
+//   };
+
+//   changePage = (pageName) => {
+//     this.setState({ currentPage: pageName });
+//   };
+
+//   render() {
+//     return (
+//       <div className="flex flex-col min-h-screen bg-gray-50">
+//         <Header changePage={this.changePage} />
+        
+//         <main className="flex-grow">
+//           {this.state.currentPage === "home" && <Home />}
+//           {this.state.currentPage === "app" && <App />}
+//           {this.state.currentPage === "product" && <ProductApp />}
+//           {this.state.currentPage === "pokemon" && <AppPokemon />}
+//           {this.state.currentPage === "review" && <ReviewPage />}
+//         </main>
+
+//         <Footer />
+//       </div>
+//     );
+//   }
+// }
+
