1665a5a4e72978c84f719f02134c8db207f17357
Akmal Rian <akmalrian@gmail.com>
2026-04-02 22:55:54 +0700

    feat : add component header and footer for all minitask

---
+++ src/component/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => (
+  <footer className="bg-gray-800 text-gray-300 py-6 mt-10 bottom-0 w-full">
+    <div className="container mx-auto text-center">
+      <p>&copy; 2026 App. Minitask React JS.</p>
+    </div>
+  </footer>
+);
+export default Footer;
---
+++ src/component/Header.jsx
@@ -0,0 +1,95 @@
+import { NavLink } from "react-router";
+
+function Header() {
+  return (
+    <header className="bg-red-600 text-white top-0">
+      <nav className="container w-full px-6 py-4 flex justify-between items-center">
+        <h1 className="font-bold italic">Minitask React JS</h1>
+        <ul className="flex space-x-6">
+          <li className="text-xl font-bold">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) => {
+                return isActive
+                  ? "text-blue-800"
+                  : "text-black hover:text-blue-400";
+              }}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="text-xl font-bold">
+            <NavLink
+              to={"/increase&decrease"}
+              className={({ isActive }) => {
+                return isActive
+                  ? "text-blue-800"
+                  : "text-black hover:text-blue-400";
+              }}
+            >
+              Increase & Decrease
+            </NavLink>
+          </li>
+          <li className="text-xl font-bold">
+            <NavLink
+              to={"/product"}
+              className={({ isActive }) => {
+                return isActive
+                  ? "text-blue-800"
+                  : "text-black hover:text-blue-400";
+              }}
+            >
+              Form Product
+            </NavLink>
+          </li>
+          <li className="text-xl font-bold">
+            <NavLink
+              to={"/pokemon"}
+              className={({ isActive }) => {
+                return isActive
+                  ? "text-blue-800"
+                  : "text-black hover:text-blue-400";
+              }}
+            >
+              List Pokemon
+            </NavLink>
+          </li>
+          <li className="text-xl font-bold">
+            <NavLink
+              to={"/review"}
+              className={({ isActive }) => {
+                return isActive
+                  ? "text-blue-800"
+                  : "text-black hover:text-blue-400";
+              }}
+            >
+              Review
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
+
+{
+  /* <li>
+          <button onClick={() => changePage("home")} className="hover:underline">Home</button>
+        </li>
+        <li>
+          <button onClick={() => changePage("app")} className="hover:text-red-200 transition">Increase & Decrease</button>
+        </li>
+        <li>
+          <button onClick={() => changePage("product")} className="hover:text-red-200 transition">Form Product</button>
+        </li>
+        <li>
+          <button onClick={() => changePage("pokemon")} className="hover:text-red-200 transition">List Pokemon</button>
+        </li>
+        <li>
+          <button onClick={() => changePage("review")} className="hover:underline">Reviews</button>
+        </li>
+      </ul>
+    </nav> */
+}
