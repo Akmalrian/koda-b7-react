cf1c543294748d65b2ba1dabe70caba20e123d72
Akmal Rian <akmalrian@gmail.com>
2026-04-02 22:59:34 +0700

    feat : Add home menu

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+
+const Home = () => (
+  <section className="container mx-auto px-6 py-40 text-center mb-2">
+    <h2 className="text-5xl font-extrabold text-gray-800 mb-6">Selamat Datang di Home Page</h2>
+    <p className="text-xl text-gray-600 mx-auto">
+      Minitask Koda React JS
+    </p>
+    <div className="mt-10">
+    </div>
+  </section>
+);
+
+export default Home;
